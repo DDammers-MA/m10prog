da59c83bdb8af2e056dcadd19a0ca98aa7f461e3
Daniel <33257@ma-web.nl>
2024-01-11 10:47:25 +0100

    single page project

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -8,7 +8,7 @@
         @foreach($project as $project )
             <h2>{{$project->titel}}</h2>
             <p>{{$project->description}}</p>
-          
+            <a href="{{route('project.show', $project)}}">Bekijk dit item</a>
             @endforeach
 </article>
 </section>
